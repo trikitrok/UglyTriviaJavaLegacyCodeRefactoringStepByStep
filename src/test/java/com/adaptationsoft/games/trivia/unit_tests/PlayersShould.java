9fb0aced0b49439ad02d687a384285209f7b54f5
trikitrok <trikitrok@gmail.com>
2016-02-25 10:56:15 +0100

    Added some test dependencies to the project

---
+++ src/test/java/com/adaptationsoft/games/trivia/unit_tests/PlayersShould.java
@@ -0,0 +1,11 @@
+package com.adaptationsoft.games.trivia.unit_tests;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
+public class PlayersShould {
+}
