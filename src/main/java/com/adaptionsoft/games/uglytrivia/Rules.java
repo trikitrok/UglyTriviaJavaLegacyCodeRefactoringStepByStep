cb12f063e6ce4792e51d8cdbe2bea9853f0ca3f2
trikitrok <trikitrok@gmail.com>
2016-02-25 10:26:34 +0100

    Extracted explaining helper

--- src/main/java/com/adaptionsoft/games/uglytrivia/Rules.java
+++ src/main/java/com/adaptionsoft/games/uglytrivia/Rules.java
@@ -2,10 +2,14 @@ package com.adaptionsoft.games.uglytrivia;
 
 public class Rules {
     public boolean playerShouldContinueInPenaltyBox(int roll) {
-        return roll % 2 == 0;
+        return isEven(roll);
     }
 
     public boolean playerHasWon(int pursesWon) {
         return (pursesWon == 6);
+    }
+
+    private boolean isEven(int roll) {
+        return roll % 2 == 0;
     }
 }
