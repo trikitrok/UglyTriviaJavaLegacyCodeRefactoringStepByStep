7123445cb2a0e7edd6baccd3514807c4c61c77d3
trikitrok <trikitrok@gmail.com>
2016-02-25 10:28:05 +0100

    Removed magic number

--- src/main/java/com/adaptionsoft/games/uglytrivia/Rules.java
+++ src/main/java/com/adaptionsoft/games/uglytrivia/Rules.java
@@ -1,12 +1,15 @@
 package com.adaptionsoft.games.uglytrivia;
 
 public class Rules {
+
+    public static final int NUMBER_OF_PURSES_TO_WIN = 6;
+
     public boolean playerShouldContinueInPenaltyBox(int roll) {
         return isEven(roll);
     }
 
     public boolean playerHasWon(int pursesWon) {
-        return (pursesWon == 6);
+        return (pursesWon == NUMBER_OF_PURSES_TO_WIN);
     }
 
     private boolean isEven(int roll) {
