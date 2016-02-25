43c11df0f83bbe531cf3730a79375eb87a51cd99
trikitrok <trikitrok@gmail.com>
2016-02-25 11:16:51 +0100

    Added test for selecting the next Player

--- src/test/java/com/adaptationsoft/games/trivia/unit_tests/PlayersShould.java
+++ src/test/java/com/adaptationsoft/games/trivia/unit_tests/PlayersShould.java
@@ -1,11 +1,20 @@
 package com.adaptationsoft.games.trivia.unit_tests;
 
+import com.adaptionsoft.games.uglytrivia.Player;
+import com.adaptionsoft.games.uglytrivia.Players;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
 
 public class PlayersShould {
+    @Test
+    public void select_next_player_in_a_cycle() {
+        Players players = new Players("Chet", "Pat", "Sue");
+
+        assertThat(players.next(), is(new Player("Chet")));
+        assertThat(players.next(), is(new Player("Pat")));
+        assertThat(players.next(), is(new Player("Sue")));
+        assertThat(players.next(), is(new Player("Chet")));
+    }
 }
