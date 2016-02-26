204aa8b9bdd925871c75796e183a2cd59f1e37af
trikitrok <trikitrok@gmail.com>
2016-02-26 20:38:58 +0100

    Added tests for the Rules

--- src/main/java/com/adaptionsoft/games/uglytrivia/Rules.java
+++ src/main/java/com/adaptionsoft/games/uglytrivia/Rules.java
@@ -2,14 +2,14 @@ package com.adaptionsoft.games.uglytrivia;
 
 public class Rules {
 
-    public static final int NUMBER_OF_GOLD_COINS_TO_WIN = 6;
+    public static final int NECESSARY_GOLD_COINS_NUMBER_TO_WIN = 6;
 
     public boolean playerShouldContinueInPenaltyBox(int roll) {
         return isEven(roll);
     }
 
     public boolean playerHasWon(int goldCoinsEarned) {
-        return (goldCoinsEarned == NUMBER_OF_GOLD_COINS_TO_WIN);
+        return (goldCoinsEarned == NECESSARY_GOLD_COINS_NUMBER_TO_WIN);
     }
 
     private boolean isEven(int roll) {
---
+++ src/test/java/com/adaptationsoft/games/trivia/tests/unit/RulesShould.java
@@ -0,0 +1,42 @@
+package com.adaptationsoft.games.trivia.tests.unit;
+
+import com.adaptionsoft.games.uglytrivia.Rules;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class RulesShould {
+
+    private Rules rules;
+
+    @Before
+    public void setUp() {
+        rules = new Rules();
+    }
+
+    @Test
+    public void a_player_should_continue_in_penalty_box_when_its_roll_is_even() {
+        int anyEvenRoll = 2;
+        assertThat(rules.playerShouldContinueInPenaltyBox(anyEvenRoll), is(true));
+    }
+
+    @Test
+    public void a_player_should_not_continue_in_penalty_box_when_its_roll_is_odd() {
+        int anyOddRoll = 3;
+        assertThat(rules.playerShouldContinueInPenaltyBox(anyOddRoll), is(false));
+    }
+
+    @Test
+    public void a_player_has_won_when_it_has_the_necessary_gold_coins_number() {
+        int playersNumberOfGoldCoins = Rules.NECESSARY_GOLD_COINS_NUMBER_TO_WIN;
+        assertThat(rules.playerHasWon(playersNumberOfGoldCoins), is(true));
+    }
+
+    @Test
+    public void a_player_has_not_won_when_it_does_not_have_the_necessary_gold_coins_number() {
+        int playersNumberOfGoldCoins = Rules.NECESSARY_GOLD_COINS_NUMBER_TO_WIN - 1;
+        assertThat(rules.playerHasWon(playersNumberOfGoldCoins), is(false));
+    }
+}
