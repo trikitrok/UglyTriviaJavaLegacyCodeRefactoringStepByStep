3b35ad953268e5fc533c3524a61e03397664a0b3
trikitrok <trikitrok@gmail.com>
2016-02-26 06:10:25 +0100

    Some renaming and made a method private

--- src/main/java/com/adaptionsoft/games/uglytrivia/Board.java
+++ src/main/java/com/adaptionsoft/games/uglytrivia/Board.java
@@ -3,30 +3,30 @@ package com.adaptionsoft.games.uglytrivia;
 import java.util.List;
 
 public class Board {
-    private final int BOARDS_PLACES_NUMBER = 12;
+    private final int BOARD_LENGTH = 12;
     private List<BoardLocation> boardLocations;
 
     public Board(List<BoardLocation> boardLocations) {
         this.boardLocations = boardLocations;
     }
 
-    public int getPlaceAhead(int currentPlace, int places) {
-        int place = currentPlace + places;
-        if (place > BOARDS_PLACES_NUMBER - 1) {
-            place = place - BOARDS_PLACES_NUMBER;
-        }
-        return place;
+    public BoardLocation boardLocationAt(int position) {
+        return boardLocations.get(position);
     }
 
-    public BoardLocation boardLocationAt(int place) {
-        return boardLocations.get(place);
-    }
-
-    public BoardLocation getLocationAhead(BoardLocation currentBoardLocation, int places) {
-        return boardLocationAt(getPlaceAhead(currentBoardLocation.position(), places));
+    public BoardLocation getLocationAhead(BoardLocation currentBoardLocation, int positions) {
+        return boardLocationAt(getPositionAhead(currentBoardLocation.position(), positions));
     }
 
     public BoardLocation getStart() {
         return boardLocations.get(0);
+    }
+
+    private int getPositionAhead(int currentPositions, int positions) {
+        int position = currentPositions + positions;
+        if (position > BOARD_LENGTH - 1) {
+            position = position - BOARD_LENGTH;
+        }
+        return position;
     }
 }
