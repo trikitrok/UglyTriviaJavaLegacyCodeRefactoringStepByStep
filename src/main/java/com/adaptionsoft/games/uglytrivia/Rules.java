3894b77a227ff4912df02c0c5a06a2e53165985d
trikitrok <trikitrok@gmail.com>
2016-02-26 12:42:46 +0100

    Renamed some constants and parameters

--- src/main/java/com/adaptionsoft/games/uglytrivia/Rules.java
+++ src/main/java/com/adaptionsoft/games/uglytrivia/Rules.java
@@ -2,14 +2,14 @@ package com.adaptionsoft.games.uglytrivia;
 
 public class Rules {
 
-    public static final int NUMBER_OF_PURSES_TO_WIN = 6;
+    public static final int NUMBER_OF_GOLD_COINS_TO_WIN = 6;
 
     public boolean playerShouldContinueInPenaltyBox(int roll) {
         return isEven(roll);
     }
 
-    public boolean playerHasWon(int pursesWon) {
-        return (pursesWon == NUMBER_OF_PURSES_TO_WIN);
+    public boolean playerHasWon(int goldCoinsEarned) {
+        return (goldCoinsEarned == NUMBER_OF_GOLD_COINS_TO_WIN);
     }
 
     private boolean isEven(int roll) {
