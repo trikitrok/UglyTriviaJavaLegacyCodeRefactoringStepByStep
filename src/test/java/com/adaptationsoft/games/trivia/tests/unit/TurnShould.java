57d8c1daea4fd366696b68d59961aa4e6b72d541
trikitrok <trikitrok@gmail.com>
2016-02-26 06:58:37 +0100

    Removed some duplication

--- src/test/java/com/adaptationsoft/games/trivia/tests/unit/TurnShould.java
+++ src/test/java/com/adaptationsoft/games/trivia/tests/unit/TurnShould.java
@@ -2,6 +2,7 @@ package com.adaptationsoft.games.trivia.tests.unit;
 
 import com.adaptationsoft.games.trivia.tests.helpers.BoardFactory;
 import com.adaptionsoft.games.uglytrivia.*;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.mockito.Mockito.doReturn;
@@ -9,21 +10,32 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 public class TurnShould {
-    @Test
-    public void notify_roll_result() {
-        int rollResult = 3;
-        Dice dice = mock(Dice.class);
-        doReturn(rollResult).when(dice).roll();
-        GameNotifications gameNotifications = mock(GameNotifications.class);
-        Jury jury = mock(Jury.class);
 
-        Turn turn = new Turn(
+    private Dice dice;
+    private GameNotifications gameNotifications;
+    private Player player;
+    private int rollResult;
+    private Jury jury;
+    private Turn turn;
+
+    @Before
+    public void setUp() {
+        rollResult = 3;
+        dice = mock(Dice.class);
+        doReturn(rollResult).when(dice).roll();
+        gameNotifications = mock(GameNotifications.class);
+        player = new Player("koko", gameNotifications);
+        jury = mock(Jury.class);
+        turn = new Turn(
             dice,
             BoardFactory.create(),
             new Rules(),
             gameNotifications,
             jury);
+    }
 
+    @Test
+    public void notify_roll_result() {
         turn.play(new Player("koko", gameNotifications));
 
         verify(gameNotifications).diceRollWas(rollResult);
@@ -31,20 +43,6 @@ public class TurnShould {
 
     @Test
     public void tells_the_jury_to_reward_the_players_answer() {
-        GameNotifications gameNotifications = mock(GameNotifications.class);
-        Player player = new Player("koko", gameNotifications);
-        int rollResult = 3;
-        Dice dice = mock(Dice.class);
-        doReturn(rollResult).when(dice).roll();
-        Jury jury = mock(Jury.class);
-
-        Turn turn = new Turn(
-            dice,
-            BoardFactory.create(),
-            new Rules(),
-            gameNotifications,
-            jury);
-
         turn.play(player);
 
         verify(jury).rewardAnswer(player);
